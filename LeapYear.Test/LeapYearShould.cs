3442da965efeca48d7b35311228f1c7d57939607
Felipe Santana <fesantana@aidacanarias.com>
2022-11-16 20:44:03 +0000

    Red: say_that_1800_is_not_leap_year()

--- LeapYear.Test/LeapYearShould.cs
+++ LeapYear.Test/LeapYearShould.cs
@@ -36,5 +36,14 @@ namespace LeapYear.Test {
             isLeapYear.Should().BeFalse();
         }
 
+        [Test]
+        public void say_that_1800_is_not_leap_year() {
+            var year = 1800;
+
+            var isLeapYear = LeapYear.IsLeap(year);
+
+            isLeapYear.Should().BeFalse();
+        }
+
     }
 }
