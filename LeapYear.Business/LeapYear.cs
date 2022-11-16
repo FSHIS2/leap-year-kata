809ebb6a5146fc55789ec2887f3be3dce38d177d
Felipe Santana <fesantana@aidacanarias.com>
2022-11-16 20:45:09 +0000

    Green: say_that_1800_is_not_leap_year()

--- LeapYear.Business/LeapYear.cs
+++ LeapYear.Business/LeapYear.cs
@@ -2,7 +2,7 @@
     public class LeapYear {
 
         public static bool IsLeap(int year) {
-            if (year == 1700) return false;
+            if (year == 1700 || year == 1800) return false;
             return true;
         }
 
