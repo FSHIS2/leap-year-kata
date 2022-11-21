1c24614b6bf17422d25233f3c73398415b58d17a
Felipe Santana <fesantana@aidacanarias.com>
2022-11-21 13:22:38 +0000

    Refactoring: LeapYear made more readable

--- LeapYear.Business/LeapYear.cs
+++ LeapYear.Business/LeapYear.cs
@@ -2,9 +2,21 @@
     public class LeapYear {
 
         public static bool IsLeap(int year) {
-            if (year % 400 == 0) return true;
-            if (year % 4 == 0 && year % 100 != 0) return true;
+            if (IsDivisibleBy400(year)) return true;
+            if (IsDivisibleBy4(year) && !IsDivisibleBy100(year)) return true;
             return false;
+        }
+
+        public static bool IsDivisibleBy4(int year) {
+            return year % 4 == 0;
+        }
+
+        public static bool IsDivisibleBy100(int year) {
+            return year % 100 == 0;
+        }
+
+        public static bool IsDivisibleBy400(int year) {
+            return year % 400 == 0;
         }
 
     }
